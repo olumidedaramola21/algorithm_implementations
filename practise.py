ff6be44379171be125767f84303316f6e48454a1
Olumide Daramola <olumidedaramola426@gmail.com>
2023-07-30 19:58:16 +0100

    first commit

---
+++ practise.py
@@ -0,0 +1,89 @@
+# message = "Hello!"
+# print(message)
+
+# firstName = "olumide"
+# print("Hello " + firstName.title() + ", would you like to learn some python today!")
+# print(4 // 2)
+# print(4 / 2)
+# print(9 / 2)
+# print(9 // 2)
+# favorite_number = 14
+# print("My Favorite number is: " + str(favorite_number))
+
+# LISTS
+# bicycles = ["trek", "cannondale", "redline", "specialized"]
+# print(bicycles)
+# print(bicycles[0])
+# print(bicycles[3])
+# print(bicycles[len(bicycles)])
+# friends = ["timi", "olumide", "anana"]
+# print("My best friend is " + friends[0])
+# print("This is my childhood friend " + friends[1])
+# motorcycles = ["honda", "yamaha", "suzuki", "ducati"]
+# # last_owned = motorcycles.pop()
+# # print("The last motorcycle i owned was a " + last_owned + ".")
+# too_expensive = "ducati"
+# motorcycles.remove(too_expensive)
+# print(motorcycles)
+# print("A " + too_expensive.title() + " is too expensive for me.")
+
+
+# list = ["cr7", "messi", "rooney", "snachez"]
+# print("I invite to my meeting " + list[0])
+# print("I invite to my meeting " + list[1])
+# print("I invite to my meeting " + list[2])
+# print(list[2] + " could not attend my meeting.")
+# list[2] = "rashford"
+# print("I invite to my meeting " + list[0])
+# print("I invite to my meeting " + list[1])
+# print("I invite to my meeting " + list[2])
+# print("I have found a bigger dinner table.")
+
+# mid = len(list) // 2
+
+# list.insert(0, "mbappe")
+# list.insert(mid, "bruno")
+# list.append("hakimi")
+
+# print(list)
+
+# cars = ["bmw", "audi", "toyota", "subaru"]
+# cars.sort()
+# print(cars)
+
+# places = ["lisbob", "london", "san-francisco", "new-york", "austria"]
+# print(places)
+# isorted = sorted(places)
+# print(isorted)
+# print(places)
+# places.reverse()
+# print(places)
+# places.reverse()
+# places.sort()
+# places.sort(reverse=True)
+
+# list = [2, 4, 1, 3, 6, 7, 9, 1]
+# list.sort()
+# print(list)
+
+# magicians = ["alice", "david", "carolina"]
+# for magician in magicians:
+#     print(magician.title() + ", that was a great trick!")
+#     print("I can't wait to see your next trick, " + magician.title() + ".\n")
+
+# food_stuff = ["rice", "yam", "beans"]
+# for food in food_stuff:
+#     print("I like " + food)
+
+# print("I really pizza.")
+
+# squares = []
+# for value in range(1, 11):
+#     square = value**2
+#     squares.append(square)
+
+# print(squares)
+# for value in range(1, 21):
+#     print(value)
+# for odd_num in range(1, 20, 3):
+#     print(odd_num)
