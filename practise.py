7192684e0d0f5f5969cd855199dfe5cc9f687aa0
Olumide Daramola <olumidedaramola426@gmail.com>
2023-08-01 13:04:05 +0100

    update

--- practise.py
+++ practise.py
@@ -179,3 +179,18 @@
 #         print("Username " + new + " already exits, enter a new username!")
 #     else:
 #         print("Username is avaible")
+
+
+# ########################ORDINAL NUMBERS######################
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# for num in numbers:
+#     if num == 1:
+#         print(str(num) + "st")
+#     elif num == 2:
+#         print(str(num) + "nd")
+#     elif num == 3:
+#         print(str(num) + "rd")
+#     else:
+#         print(str(num) + "th")
+
+# ##########################DICTIONARIES###################
