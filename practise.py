74ce4cb1e9175778f6b3c8d17cac67238723578e
Olumide Daramola <olumidedaramola426@gmail.com>
2023-08-06 11:01:26 +0100

    update

--- practise.py
+++ practise.py
@@ -428,5 +428,11 @@
 
 # INPUT
 
-message = input("Tell me something, and I will repeat it back to you: ")
-print(message)
+# message = input("Tell me something, and I will repeat it back to you: ")
+# print(message)
+
+# current_number = 1
+
+# while current_number <= 5:
+#     print(current_number)
+#     current_number += 1
