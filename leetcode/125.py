942bfd40209e334b638e375393314957f1455728
Olumide Daramola <olumidedaramola426@gmail.com>
2023-10-13 20:19:53 +0100

    add solution ii for valid palindrome

--- leetcode/125.py
+++ leetcode/125.py
@@ -6,10 +6,35 @@
 
 
 # Solution 1
+# Solution uses extra memory
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         newStr = ""
+#         for c in s:
+#             if c.isalnum():
+#                 newStr += c.lower()
+#         return newStr == newStr[::-1]
+
+
+# Solution 2
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newStr = ""
-        for c in s:
-            if c.isalnum():
-                newStr += c.lower()
-        return newStr == newStr[::-1]
+        def alphaNum(c):
+            return (
+                ord("A") <= ord(c) <= ord("Z")
+                or ord("a") <= ord(c) <= ord("z")
+                or ord("0") <= ord(c) <= ord("9")
+            )
+
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not alphaNum(s[l]):
+                l += 1
+            while r > l and not alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+
+        return True
